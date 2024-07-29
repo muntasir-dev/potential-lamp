fdbc21b0ce402fc8df6fba79512caeab9375553f
Md Muntasir Hossain <78915768+muntasir-dev@users.noreply.github.com>
2024-07-29 09:25:22 +0000

    modified:   src/components/Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -18,9 +18,8 @@ const Welcome = () => {
       <h2>Welcome to Quick Chat.</h2>
       <img src="/logo512.png" alt="ReactJs logo" width={50} height={50} />
       <p>Sign in with Google to chat with everyone.</p>
-      <button className="sign-in">
+      <button className="sign-in" onClick={signInWithGoogle}>
         <img
-          onClick={signInWithGoogle}
           src={GoogleSignin}
           alt="sign in with google"
           type="button"
