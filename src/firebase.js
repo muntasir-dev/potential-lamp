bfdd5f64b08b98e9725676bd4ad62a940be259ce
Md Muntasir Hossain <78915768+muntasir-dev@users.noreply.github.com>
2024-07-29 08:52:28 +0000

    modified:   src/firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -21,3 +21,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+export const auth = app.auth();
+export default app;
